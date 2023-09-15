2bb39b3c030775472c17b1b9270c376414aa0e6b
MendoDuke <140199052+MendoDuke@users.noreply.github.com>
2023-09-14 18:52:42 -0700

    Committing changes in Joystick_Junction_project

---
+++ joystick-junction.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyButK7uMm3_aQ9Fg2QB3Z4FHJIcsokQna4",
+  authDomain: "joystick-junction.firebaseapp.com",
+  projectId: "joystick-junction",
+  storageBucket: "joystick-junction.appspot.com",
+  messagingSenderId: "979900202205",
+  appId: "1:979900202205:web:3ee13f2d90aade6c4eaf34",
+  measurementId: "G-DQ4JM5ZECY",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
+
+let gamesPlayed = 0;
+let wins = 0;
+let losses = 0;
+
+function updateStatistics(outcome) {
+  gamesPlayed++;
+  if (outcome === "win") {
+    wins++;
+  } else if (outcome === "loss") {
+    losses++;
+  }
+
+  document.getElementById("games-played").textContent = gamesPlayed;
+  document.getElementById("wins").textContent = wins;
+  document.getElementById("losses").textContent = losses;
+}
